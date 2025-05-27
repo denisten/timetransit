2e126f43fc5f30c2e2e19a272c9357129c0a6fc0
Ten Denis <denisten>
2025-05-27 13:21:04 +0300

    updated favicon

--- FE/src/index.js
+++ FE/src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"
+import "./index.css";
 import "animate.css/animate.min.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />, root);
